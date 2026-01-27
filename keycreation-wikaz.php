fd475b509eb4a725a4892081bbc8fb55acf0398c
Muhammad Fajar <mfjr48@gmail.com>
2026-01-28 06:44:17 +0700

    add: no cache css and js

--- keycreation-wikaz.php
+++ keycreation-wikaz.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin constants
-define('WIKAZ_VERSION', '1.0.0');
+define('WIKAZ_VERSION', time());
 define('WIKAZ_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WIKAZ_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('WIKAZ_PLUGIN_BASENAME', plugin_basename(__FILE__));
