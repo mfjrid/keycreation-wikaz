99990c72ca6cfe524759ae864862ba441561eea4
Muhammad Fajar <mfjr48@gmail.com>
2026-02-16 13:27:56 +0700

    add: unique time css file

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -191,14 +191,14 @@ class Wikaz_Admin
             'wikaz-admin-style',
             WIKAZ_PLUGIN_URL . 'admin/css/admin-style2.css',
             array(),
-            WIKAZ_VERSION
+            time()
         );
 
         wp_enqueue_script(
             'wikaz-admin-script',
             WIKAZ_PLUGIN_URL . 'admin/js/admin-script2.js',
             array('jquery', 'jquery-ui-sortable', 'wp-color-picker'),
-            WIKAZ_VERSION,
+            time(),
             true
         );
 
